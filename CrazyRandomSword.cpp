2de8e73dc3b79977162f57d4d3c69ba2942b6cf8
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-01-31 22:18:17 -0500

    Created Files for New Weapons
    	Files for the new weapons created and given a header and
    	prepared for use in program.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,6 @@
+/* 
+ * File:   CrazyRandomSword.cpp
+ * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
+ * 
+ * Created on January 31, 2019, 10:09 P.M.
+ */
---
+++ CrazyRandomSword.h
@@ -0,0 +1,6 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
+ * 
+ * Created on January 31, 2019, 10:09 P.M.
+ */
