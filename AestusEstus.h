13a3d83360327d0ebc9a46d130819e86fa6a576b
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-01-31 22:30:37 -0500

    Prepped new weapons
    	New weapons have files prepped for coding. Added header with
    	development information.

---
+++ AestusEstus.h
@@ -0,0 +1,6 @@
+/* 
+ * File:   AestusEstus.h
+ * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
+ * 
+ * Created on January 31, 2019, 10:16 P.M.
+ */
---
+++ SimpleHammer.cpp
@@ -0,0 +1,6 @@
+/* 
+ * File:   SimpleHammer.cpp
+ * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
+ * 
+ * Created on January 31, 2019, 10:08 P.M.
+ */
