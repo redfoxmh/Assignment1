ded861d110a4338d8dc84666dd36c88b96b40541
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-01-31 22:48:32 -0500

    SimpleHammer Completed
    	SimpleHammer class is completed as a child class of the Weapon
    	class.

--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -4,3 +4,14 @@
  * 
  * Created on January 31, 2019, 10:08 P.M.
  */
+ 
+ #include "SimpleHammer.h"
+
+double SimpleHammer::hit(double armor)
+{
+    if (armor < 30)
+	{
+        return hitPoints;
+    }
+    return 0;
+}
--- SimpleHammer.h
+++ SimpleHammer.h
@@ -4,3 +4,27 @@
  * 
  * Created on January 31, 2019, 10:08 P.M.
  */
+ 
+ #include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEHAMMER_H
+#define SIMPLEHAMMER_H
+
+/**
+ * Defines the behavior of a Simple Hammer
+ (hitpoint = 25, ignores all armor if armor is less than 30)
+ */
+class SimpleHammer : public Weapon {
+public:
+
+    SimpleHammer() : Weapon("Simple Hammer", 25.0) { //Calls Weapon(name, hitpoints) constructor with values Simple Hammer and 25.0
+    }
+
+    virtual ~SimpleHammer() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* SIMPLEHAMMER_H */
