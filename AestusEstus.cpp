a77cc5ab833a5c6acb42ce078e4717e0cd82e796
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-01-31 22:27:33 -0500

    Revert "	Created Files for New Weapons"
    This reverts commit 2de8e73dc3b79977162f57d4d3c69ba2942b6cf8.
    
    	Forgot to create branch
    
    	Forgot to create branch before creating files. Reverting to put
    	proper changes in place.

--- AestusEstus.cpp
+++
@@ -1,6 +0,0 @@
-/* 
- * File:   AestusEstus.cpp
- * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
- * 
- * Created on January 31, 2019, 10:08 P.M.
- */
--- AestusEstus.h
+++
@@ -1,6 +0,0 @@
-/* 
- * File:   AestusEstus.h
- * Author: Luiz Hosfeld (Lh15s@my.fsu.edu(
- * 
- * Created on January 31, 2019, 10:16 P.M.
- */
