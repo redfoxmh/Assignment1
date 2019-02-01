98866c382c5d417875a5f04d8a82695e2cbbd7e4
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-01-31 22:50:52 -0500

    Added SimpleHammer to WeaponFactory
    	WeaponFactory class now generates Simple Hammers when requested.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -27,6 +27,11 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("spear") == 0) {
         return new CommonSpear();
     }
+	
+	if (name.compare("hammer") == 0)
+	{
+		return new SimpleHammer()
+	}
 
     throw "Invalid weapon";
 }
